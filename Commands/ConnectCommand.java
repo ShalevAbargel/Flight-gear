14dc5a32c70ef694960e4787f82e980883e8e2a6
Shalev Abargel <shalev25a@gmail.com>
2020-01-19 20:28:00 +0200

    Add files via upload

--- Commands/ConnectCommand.java
+++ Commands/ConnectCommand.java
@@ -7,22 +7,31 @@ import java.net.UnknownHostException;
 
 import expression.ExpressionCalculator;
 import interpreter.MyInterpreter;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 
 public class ConnectCommand implements Command{
 	
 	private static Socket client = null;
 	public static  PrintWriter out = null;
 	public static boolean stop =false;
+	public boolean connected = false;
 		@Override
 		public int doCommand(String[] line) {
-			if(line.length != 2)
-				System.out.println("Error in parameters in dataServer");
-			else
+			if(!connected)
 			{
-				String ip = line[0];
-				int port = (int)ExpressionCalculator.calc(line[1]);
-				new Thread(()->runClient(ip,port)).start();
+				if(line.length != 2)
+					System.out.println("Error in parameters in connect command");
+				else
+				{
+					String ip = line[0];
+					int port = (int)ExpressionCalculator.calc(line[1]);
+					new Thread(()->runClient(ip,port)).start();
+				}
 			}
+			else
+				System.out.println("you already connected to server, please continue");
+			
 			return 0;
 		}
 
@@ -31,6 +40,7 @@ public class ConnectCommand implements Command{
 			try {
 				client = new Socket(ip, port);
 				System.out.println("client is connected to simulator server");
+				this.connected=true;
 				out = new PrintWriter(client.getOutputStream());
 				while(!stop)
 				{
@@ -47,7 +57,9 @@ public class ConnectCommand implements Command{
 			} catch (UnknownHostException e) {
 				e.printStackTrace();
 			} catch (IOException e) {
-				e.printStackTrace();
+				System.out.println("Simulator server not open yet");
+				System.out.println("Please wait for simulator server to open and then try again to connect");
+				//e.printStackTrace();
 			}
 			
 		}
